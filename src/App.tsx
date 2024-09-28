914f74abfaa3366c5f539d20b32e5b748ca42d3f
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2024-09-28 12:35:46 -0300

    criação da estrutura e estilização do App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css'
 import styles from './app.module.css'
 import { Header } from './components/header'
 import { NewTaskForm } from './components/new-task-form'
+import clipboard from './assets/clipboard.svg'
 
 export function App() {
   return (
@@ -10,8 +11,25 @@ export function App() {
 
       <div className={styles.wrapper}>
         <NewTaskForm />
-      </div>
 
+        <div className={styles.listWrapper}>
+          <header>
+            <strong>Tarefas criadas <span>0</span></strong>
+
+            <strong>Concluidas <span>0</span></strong>
+          </header>
+
+          <main>
+            <div className={styles.emptyList}>
+              <img src={clipboard} />
+
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </main>
+        </div>
+      </div>
     </>
   )
 }
